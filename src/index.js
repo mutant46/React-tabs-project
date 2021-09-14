c227440a3be0d674dbf500ea9e6652959bf46dc3
maqbool <junaidmaqbool46@yahoo.com>
2021-09-14 02:39:58 -0700

    basic changes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 ReactDOM.render(
   <React.StrictMode>
+    <h1 className='text-center mt-10 py-10 font-black text-3xl tracking-wider'>Our Menu</h1>
     <App />
   </React.StrictMode>,
   document.getElementById("root")
