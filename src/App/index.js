9232e5ba0ab6db1db761fef058ab5302d4005d7b
junai <lucifermjm@gmail.com>
2021-09-16 13:30:48 +0500

    Final commit

--- src/App/index.js
+++ src/App/index.js
@@ -1,33 +1,56 @@
-import React, { useState } from "react";
-import UseFetch from "../custom-hooks";
+import React, { useState, useEffect } from "react";
+import { useFetch } from "../custom-hooks";
 import Loading from "../loading";
+const url = "http://mutant46.pythonanywhere.com/react-couse-api/";
 
 const Index = () => {
-  const { isLoading, products } = UseFetch();
-  console.log(products);
+  const { isLoading, products } = useFetch(url);
   const [menu, setMenu] = useState(products);
+
+  useEffect(() => {
+    setMenu(products);
+  }, [products]);
+
+  const setProducts = (category) => {
+    let newProducts = products.filter(
+      (product) => product.category === category
+    );
+    setMenu(newProducts);
+  };
 
   return (
     <div>
       <ul className="w-1/2  flex justify-center text-lg tabs font-bold m-auto ">
-        <li className="mx-5 cursor-pointer hover:bg-green-200 px-3 py-1 rounded-sm selected">
+        <li
+          className="mx-5 cursor-pointer hover:bg-green-200 px-3 py-1 rounded-sm"
+          onClick={() => setMenu(products)}
+        >
           All
         </li>
-        <li className="mx-5 cursor-pointer px-3 py-1 rounded-sm hover:bg-green-200">
+        <li
+          className="mx-5 cursor-pointer px-3 py-1 rounded-sm hover:bg-green-200"
+          onClick={() => setProducts("breakfast")}
+        >
           Breakfast
         </li>
-        <li className="mx-5 cursor-pointer px-3 py-1 rounded-sm hover:bg-green-200">
+        <li
+          className="mx-5 cursor-pointer px-3 py-1 rounded-sm hover:bg-green-200"
+          onClick={() => setProducts("lunch")}
+        >
           Lunch
         </li>
-        <li className="mx-5 cursor-pointer px-3 py-1 rounded-sm hover:bg-green-200">
+        <li
+          className="mx-5 cursor-pointer px-3 py-1 rounded-sm hover:bg-green-200"
+          onClick={() => setProducts("shakes")}
+        >
           Shakes
         </li>
       </ul>
       {isLoading && <Loading />}
       <div className="menu__wrapper flex w-11/12 mx-auto mt-10 justify-between mx-auto flex-wrap">
         {!isLoading &&
-          products.map((item) => {
-            const { id, title, category, price, img, desc } = item;
+          menu.map((item) => {
+            const { id, title, price, img, desc } = item;
             return (
               <div
                 className="menu__item py-5 my-5 overflow-hidden md:w-6/12 md:flex"
--- src/custom-hooks/index.js
+++ src/custom-hooks/index.js
@@ -1,23 +1,20 @@
 import { useState, useEffect } from "react";
-const url = "http://mutant46.pythonanywhere.com/react-couse-api/";
 
-const useFetch = () => {
-  const [products, setProducts] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
 
-  const getProducts = async () => {
-    const response = await fetch(url);
-    const data = await response.json();
-    setProducts(data);
-    console.log(data);
-    setIsLoading(false);
-  };
+export const useFetch = (url) => {
+    const [products, setProducts] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    getProducts();
-  }, []);
+    const getProducts = async() => {
+        const response = await fetch(url);
+        const data = await response.json();
+        setProducts(data);
+        setIsLoading(false);
+    };
 
-  return { isLoading, products };
+    useEffect(() => {
+        getProducts();
+    }, [url]);
+
+    return { isLoading, products };
 };
-
-export default useFetch;
